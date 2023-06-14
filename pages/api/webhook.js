20fed1ae2e16458a7a34840f46203d3fd58bcf2d
DanupolDev <jom.namom@gmail.com>
2023-06-14 14:46:27 +0700

    seting webhook2

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -21,10 +21,37 @@ export default async function handler(req, res) {
 
   // Handle the event
 
+  const data = event.data.object;
+  const orderId = data.metadata.orderID;
+  const paid = data.payment_status === "paid";
+  if (orderId && paid) {
+    await Order.findByIdAndUpdate(orderId, {
+      paid: true,
+    });
+  }
   console.log(`Unhandled event type ${event.type}`);
 
   res.status(200).send("OK");
 }
+
+// switch (event.type) {
+//   case "checkout.session.completed":
+//     const data = event.data.object;
+//     // console.log(data);
+//     const orderId = data.metadata.orderID;
+//     const paid = data.payment_status === "paid";
+//     // console.log("pay:", paid);
+//     if (orderId && paid) {
+//       await Order.findByIdAndUpdate(orderId, {
+//         paid: true,
+//       });
+//     }
+//     // Then define and call a function to handle the event payment_intent.succeeded
+//     break;
+//   // ... handle other event types
+//   default:
+//     console.log(`Unhandled event type ${event.type}`);
+// }
 
 export const config = {
   api: { bodyParser: false },
